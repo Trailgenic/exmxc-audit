750e0537331e3b4df3277abb3d01f13a97e7e1c2
Trailgenic <mike@trailgenic.com>
2025-12-11 16:08:23 -0800

    Update entity.ts

--- src/api/internal/entity.ts
+++ src/api/internal/entity.ts
@@ -1,1 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { railwayClient } from "@/lib/railwayClient";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // TODO: Validate entityId
+  // TODO: Call Railway GET /entities/:id (later)
+  // TODO: Return internal EEI details (tiers + 13 signals + diagnostics)
+}
 
