1de62c857a64ff96f60c5b270471de10449f0f5e
Trailgenic <mike@trailgenic.com>
2025-12-11 16:07:36 -0800

    Update start.ts

--- src/api/eei/start.ts
+++ src/api/eei/start.ts
@@ -1,1 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { railwayClient } from "@/lib/railwayClient";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // TODO: Validate input
+  // TODO: Call Railway POST /jobs
+  // TODO: Return jobId
+}
 
