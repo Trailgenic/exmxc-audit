b648e652e2c42301108e714363b7bd84e63eca44
Trailgenic <mike@trailgenic.com>
2025-12-11 16:14:21 -0800

    Update index.ts

--- eei-crawler/src/crawler/index.ts
+++ eei-crawler/src/crawler/index.ts
@@ -1,1 +1,8 @@
+export async function runCrawler(jobId: string, url: string) {
+  // TODO: multi-surface orchestration
+  // TODO: fetch homepage → about → article pages
+  // TODO: run signals
+  // TODO: aggregate tiers + band
+  // TODO: save entity + diagnostics
+}
 
