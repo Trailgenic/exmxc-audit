bf2f127cc3f0a096b95df8b801ca1b6f6c96061f
Trailgenic <mike@trailgenic.com>
2025-12-11 16:09:06 -0800

    Update type.ts

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,1 +1,19 @@
+export interface JobStartResponse {
+  jobId: string;
+}
+
+export interface PublicResult {
+  url: string;
+  tiers: {
+    tier1: number;
+    tier2: number;
+    tier3: number;
+  };
+  band: string;
+}
+
+export interface InternalResult {
+  entityId: string;
+  diagnostics: any; // TODO: refine later
+}
 
