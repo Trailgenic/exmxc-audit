e8d63617b96e819fbdca1570f9a0d2951fb3754b
Trailgenic <mike@trailgenic.com>
2025-12-11 16:13:57 -0800

    Update jobs.ts

--- eei-crawler/src/api/jobs.ts
+++ eei-crawler/src/api/jobs.ts
@@ -1,1 +1,16 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.post('/jobs', async (c) => {
+  // TODO: Create job, trigger crawler
+  return c.json({ jobId: "TODO" });
+});
+
+app.get('/jobs/:jobId', async (c) => {
+  // TODO: Return job status + publicResult
+  return c.json({ status: "pending" });
+});
+
+export default app;
 
