4b3ee80fdf7ed04d248e55ff415e960e14db517e
Trailgenic <mike@trailgenic.com>
2025-12-11 16:20:56 -0800

    Update jobs.ts

--- eei-crawler/src/db/jobs.ts
+++ eei-crawler/src/db/jobs.ts
@@ -1,1 +1,13 @@
+export async function createJob(url: string) {
+  // TODO: insert into jobs table
+}
+
+export async function updateJob(jobId: string, data: any) {
+  // TODO: update job row
+}
+
+export async function getJob(jobId: string) {
+  // TODO: get job record
+  return null;
+}
 
