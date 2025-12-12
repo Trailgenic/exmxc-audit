7ca63633e6ba017be5ea6dfc2559608843e5f5dc
Trailgenic <mike@trailgenic.com>
2025-12-11 16:21:14 -0800

    Update entities.ts

--- eei-crawler/src/db/entities.ts
+++ eei-crawler/src/db/entities.ts
@@ -1,1 +1,9 @@
+export async function saveEntity(data: any) {
+  // TODO: save entity + diagnostics
+}
+
+export async function getEntity(entityId: string) {
+  // TODO: fetch entity + diagnostic JSON
+  return null;
+}
 
