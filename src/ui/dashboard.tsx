74b0a2d377ab799b28ce8cd3f9269f4c0d741f11
Trailgenic <mike@trailgenic.com>
2025-12-11 16:09:33 -0800

    Update dashboard.tsx

--- src/ui/dashboard.tsx
+++ src/ui/dashboard.tsx
@@ -1,1 +1,10 @@
+export default function Dashboard() {
+  // TODO: Internal dashboard UI
+  // TODO: Fetch internal entity data
+  return (
+    <div>
+      <h1>exmxc Internal Dashboard</h1>
+    </div>
+  );
+}
 
