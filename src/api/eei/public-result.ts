c46149d3e7ffe668c1f85da0d8b809687bddcdf0
Trailgenic <mike@trailgenic.com>
2025-12-11 16:07:56 -0800

    Update public-result.ts

--- src/api/eei/public-result.ts
+++ src/api/eei/public-result.ts
@@ -1,1 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { railwayClient } from "@/lib/railwayClient";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // TODO: Validate jobId
+  // TODO: Call Railway GET /jobs/:jobId
+  // TODO: Return publicResult
+}
 
