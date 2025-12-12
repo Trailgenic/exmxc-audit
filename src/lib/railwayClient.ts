5b7a2e362721d4a1f92ccbcedd5062653695215f
Trailgenic <mike@trailgenic.com>
2025-12-11 16:08:45 -0800

    Update railwayClient.ts

--- src/lib/railwayClient.ts
+++ src/lib/railwayClient.ts
@@ -1,1 +1,14 @@
+export const railwayClient = {
+  postJob: async (url: string) => {
+    // TODO: Implement POST /jobs
+  },
+
+  getJob: async (jobId: string) => {
+    // TODO: Implement GET /jobs/:jobId
+  },
+
+  getEntity: async (entityId: string) => {
+    // TODO: Implement GET /entities/:id (later)
+  }
+};
 
