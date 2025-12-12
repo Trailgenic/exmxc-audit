7e455e11fd758415257c5d2e982b8c750850c73d
Trailgenic <mike@trailgenic.com>
2025-12-11 18:15:59 -0800

    Delete src/api/internal/entity.ts

--- src/api/internal/entity.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { railwayClient } from "@/lib/railwayClient";
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // TODO: Validate entityId
-  // TODO: Call Railway GET /entities/:id (later)
-  // TODO: Return internal EEI details (tiers + 13 signals + diagnostics)
-}
-
